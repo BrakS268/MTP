8ec38f0ab54da60a6368ecd76bfe9b6a567faf66
BrakS268 <maks.chuev.05@mail.ru>
2025-09-13 00:42:35 +0300

    pre flow2

--- First.py
+++ First.py
@@ -2,10 +2,6 @@ print("Hello Git")
 
 print("Файл изменился")
 
-print("Изменение в Main")
-
 print("Изменение в Feature")
 
-print("Flake8 check")
-
-print("Проверка")
+print("Изменение для Pull Request")
