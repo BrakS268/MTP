74b18dee47890c07529079af7130a21093c29711
BrakS268 <maks.chuev.05@mail.ru>
2025-10-06 15:39:07 +0300

    Multiplication Table

---
+++ MultiplicationTable.py
@@ -0,0 +1,8 @@
+def MulitiplicationTable(n, m):
+    for i in range(n, m + 1):
+        for j in range(n, m + 1):
+            print(f"{i * j:3}", end=" ")
+        print()
+
+
+MulitiplicationTable(1, 10)
