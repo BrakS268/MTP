54711c4207f9290aa0aeef709c3e347510b510f1
BrakS268 <maks.chuev.05@mail.ru>
2025-10-07 17:05:07 +0300

    Find Min Value 2

--- FindMinValue.py
+++ FindMinValue.py
@@ -1,8 +1,13 @@
+def find_min_value(values):
+    minValue = values[0]
+
+    for value in values:
+        if value < minValue:
+            minValue = value
+
+    return minValue
+
+
 values = [99, 150, 41, 2022, 104, 23, 134, 221]
-minValue = values[0]
-
-for value in values:
-    if value < minValue:
-        minValue = value
-
-print(minValue)
+result = find_min_value(values)
+print(result)
